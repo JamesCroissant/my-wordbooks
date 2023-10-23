db74a28763d4e769537c5d65a5c21dd373e956a1
Yu Hamada <yuham1014@gmail.com>
2023-10-23 00:52:05 -0700

    fixed css styling in Home.jsx

--- my-wordbooks-frontend/src/pages/home/Home.jsx
+++ my-wordbooks-frontend/src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import logo from "../../images/logo-mywordbooks.png";
 
 const Home = () => {
   return (
-    <div id="home" className="my-5">
+    <div id="home" className="container">
       <main>
         <div className="px-4 text-center">
           <div className="HomeTop">
@@ -14,7 +14,7 @@ const Home = () => {
           </div>
           <div className="homeCenter">
             <img src={logo} alt="my-wordbooks-logo" className="homeLogo" />
-            <p className="fs-4">分からない単語と意味を入力して自分だけのオリジナル単語帳を作ろう！</p>
+            <p className="fs-4">分からない単語と意味を入力して、自分だけのオリジナル単語帳を作ろう！</p>
             <p className="fs-4">Create your own original wordbooks by entering words and meanings you don't understand!</p>
           </div>
           <div className="homeBottom">
